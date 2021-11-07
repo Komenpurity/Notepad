214bc06e9f54805b50b20f9f59807497eca9cb4c
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-11-07 07:08:06 +0300

    added form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NoteFormComponent } from './note-form/note-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteFormComponent
   ],
   imports: [
     BrowserModule
